c2f306baf0670cb95e74e1bbe1b8e7276a7ddfbe
Michael Fabiyi <michael.fabiyi@venturegardengroup.com>
2021-05-11 00:24:53 +0100

    Initial Commit

---
+++ src/Launcher.ts
@@ -0,0 +1,20 @@
+import { Server } from './Server/Server';
+
+class Launcher {
+
+    //instance variables
+    private name: string;
+    private server: Server;
+
+    constructor(){
+        this.server = new Server();
+    }
+
+    public launchApp(){
+        console.log('started app');
+        this.server.createServer();
+
+    }
+}
+
+new Launcher().launchApp();
---
+++ src/Server/Server.ts
@@ -0,0 +1,13 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
+export class Server {
+
+    public createServer() {
+        createServer((req: IncomingMessage, res: ServerResponse) => {
+                console.log('got request from: ' + req.url);
+                res.end();
+            }
+        ).listen(8080);
+        console.log('server started');
+    }
+}
